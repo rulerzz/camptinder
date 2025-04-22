02c2b4fed287d336eda4c24ab9c4ad125df9c49e
Mayank Kapri <rulerzz@users.noreply.github.com>
2025-04-22 13:14:05 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['camptinder.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
